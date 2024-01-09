93524cef66b0e77c178a4bce7bc0c9a30e6960a6
Fatih Kabay <mfthkabay@gmail.com>
2024-01-09 20:25:26 +0300

    finish project

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,25 +1,36 @@
 import React, { useState, useRef } from "react";
 import "./TicTacToe.css";
-import circle_icon from "../assets/circle_icon.png";
-import cross_icon from "../assets/cross_icon.png";
+import circle from "../assets/circle.png";
+import cross from "../assets/cross.png";
 
 let data = ["", "", "", "", "", "", "", "", ""];
 
 function TicTacToe() {
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState(false);
-  let titleRef = useRef();
+  let titleRef = useRef(null);
+  let box1 = useRef(null);
+  let box2 = useRef(null);
+  let box3 = useRef(null);
+  let box4 = useRef(null);
+  let box5 = useRef(null);
+  let box6 = useRef(null);
+  let box7 = useRef(null);
+  let box8 = useRef(null);
+  let box9 = useRef(null);
+
+  let boxArray = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
   const toggle = (e, num) => {
     if (lock) {
       return 0;
     }
     if (count % 2 === 0) {
-      e.target.innerHTML = `<img src='${cross_icon}'>`;
+      e.target.innerHTML = `<img src='${cross}'>`;
       data[num] = "x";
       setCount(++count);
     } else {
-      e.target.innerHTML = `<img src='${circle_icon}'>`;
+      e.target.innerHTML = `<img src='${circle}'>`;
       data[num] = "o";
       setCount(++count);
     }
@@ -29,37 +40,53 @@ function TicTacToe() {
 
   const chekWin = () => {
     if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
-      won(data);
+      won(data[2]);
     }
     else if(data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
-      won(data);
+      won(data[5]);
     }
     else if(data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     }
     else if(data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
-      won(data);
+      won(data[6]);
     }
     else if(data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
-      won(data);
+      won(data[7]);
     }
     else if(data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     }
     else if(data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
-      won(data);
+      won(data[8]);
     }
     else if(data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
-      won(data);
+      won(data[2]);
     }
     else if(data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
-      won(data);
+      won(data[6]);
     }
   };
 
   const won = (winner) => {
     setLock(true);
+    if(winner === "x") {
+      titleRef.current.innerHTML = `Congratulations: <img id="ee" src='${cross}' Win>`;
+    }
+    else {
+      titleRef.current.innerHTML = `Congratulations: <img src='${circle}'> Win`;
+
+    }
   };
+
+  const reset = () => {
+    setLock(false);
+    data = ["", "", "", "", "", "", "", "", ""];
+    titleRef.current.innerHTML = 'Tic Tac Toe in <span>React</span>'
+    boxArray.map((e) => (
+      e.current.innerHTML = ""
+    ))
+  }
 
   return (
     <div className="container">
@@ -70,18 +97,21 @@ function TicTacToe() {
         <div className="row1">
           <div
             className="boxes"
+            ref={box1}
             onClick={(e) => {
               toggle(e, 0);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box2}
             onClick={(e) => {
               toggle(e, 1);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box3}
             onClick={(e) => {
               toggle(e, 2);
             }}
@@ -90,18 +120,21 @@ function TicTacToe() {
         <div className="row2">
           <div
             className="boxes"
+            ref={box4}
             onClick={(e) => {
               toggle(e, 3);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box5}
             onClick={(e) => {
               toggle(e, 4);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box6}
             onClick={(e) => {
               toggle(e, 5);
             }}
@@ -110,25 +143,28 @@ function TicTacToe() {
         <div className="row3">
           <div
             className="boxes"
+            ref={box7}
             onClick={(e) => {
               toggle(e, 6);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box8}
             onClick={(e) => {
               toggle(e, 7);
             }}
           ></div>
           <div
             className="boxes"
+            ref={box9}
             onClick={(e) => {
               toggle(e, 8);
             }}
           ></div>
         </div>
       </div>
-      <button className="reset">Reset</button>
+      <button className="reset" onClick={() => {reset()}}>Reset</button>
     </div>
   );
 }
